6ccd02142441d5fd5a6d4859ca2bb5ac3b2c2686
Takács Mandula <mandula.fabo@gmail.com>
2025-09-04 09:31:41 +0200

    Add eager loading to img

--- fabo-website/src/App.jsx
+++ fabo-website/src/App.jsx
@@ -419,7 +419,7 @@ const App = () => {
           <img src="https://images.unsplash.com/photo-1501504905252-473c47e087f8?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt="Teamarbeit im Finanzwesen" 
             className="w-full h-full object-contain rounded-2xl border object-cover w-full h-72" 
-            loading="lazy" />
+            loading="eager" />
         </div>
       </section>
 
