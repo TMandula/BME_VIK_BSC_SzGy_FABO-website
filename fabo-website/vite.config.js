1d02e3929459a7739469eeb55eed14be2a178607
Takács Mandula <mandula.fabo@gmail.com>
2025-09-30 11:05:59 +0200

    Add sitemap

--- fabo-website/vite.config.js
+++ fabo-website/vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteSitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    viteSitemap({
+      hostname: 'https://faboaccounting.com',
+      dynamicRoutes: [
+        '/about',
+        '/services',
+        '/technologies',
+        '/contact',
+        '/imprint',
+        '/privacy-policy',
+      ],
+      outDir: 'dist',
+    }),
+  ],
 })
